3d1ab4b79dfe1bd06eb67e98396495bfb7f53906
liam.gallagher <l00103249@atu.ie>
2023-01-04 19:02:11 +0000

    Walkthrough 2 Complete along with a bugfix

---
+++ Walkthrough2/Tests.ps1
@@ -0,0 +1,29 @@
+$Variable1 = 12
+$Variable2 = 32
+if ( $Variable1 -ne $Variable2 )
+{
+ Write-Output "The condition was true"
+}
+
+$day = 3
+if ( $day -eq 0 ) { $result = 'Sunday' }
+elseif ( $day -eq 1 ) { $result = 'Monday' }
+elseif ( $day -eq 2 ) { $result = 'Tuesday' }
+elseif ( $day -eq 3 ) { $result = 'Wednesday' }
+elseif ( $day -eq 4 ) { $result = 'Thursday' }
+elseif ( $day -eq 5 ) { $result = 'Friday' }
+elseif ( $day -eq 6 ) { $result = 'Saturday' }
+$result
+
+$day = 4
+switch ( $day )
+{
+ 0 { $result = 'Sunday' }
+ 1 { $result = 'Monday' }
+ 2 { $result = 'Tuesday' }
+ 3 { $result = 'Wednesday' }
+ 4 { $result = 'Thursday' }
+ 5 { $result = 'Friday' }
+ 6 { $result = 'Saturday' }
+}
+$result
